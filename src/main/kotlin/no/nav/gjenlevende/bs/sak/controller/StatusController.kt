7a7eecdbbd6f367b24c4da836e1f9bacd11ff46b
krikof-nav <kristian.kofoed@nav.no>
2025-11-04 10:38:53 +0100

    Fjerner litt støy fra StatusController.kt

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/controller/StatusController.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/controller/StatusController.kt
@@ -1,10 +1,7 @@
 package no.nav.gjenlevende.bs.sak.controller
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping

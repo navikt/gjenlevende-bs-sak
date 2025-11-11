e2ffd099785f976d8a19713d07282f1504b348c9
krikof-nav <kristian.kofoed@nav.no>
2025-11-11 12:07:29 +0100

    Legger til propagerings filter for token bytte mot gjenlevende-bs-infotrygd.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/config/WebClientConfig.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/config/WebClientConfig.kt
@@ -3,6 +3,10 @@ package no.nav.gjenlevende.bs.sak.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -16,5 +20,25 @@ open class WebClientConfig {
             .builder()
             .baseUrl(infotrygdUrl)
             .defaultHeader("Content-Type", "application/json")
+            .filter(tokenPropageringFilter())
             .build()
+
+    private fun tokenPropageringFilter(): ExchangeFilterFunction =
+        ExchangeFilterFunction { request, next ->
+            val authentication = SecurityContextHolder.getContext().authentication
+
+            if (authentication is JwtAuthenticationToken) {
+                val token = authentication.token.tokenValue
+
+                val mutertRequest =
+                    ClientRequest
+                        .from(request)
+                        .header("Authorization", "Bearer $token")
+                        .build()
+
+                next.exchange(mutertRequest)
+            } else {
+                next.exchange(request)
+            }
+        }
 }

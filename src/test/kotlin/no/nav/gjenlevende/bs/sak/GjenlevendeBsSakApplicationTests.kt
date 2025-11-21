3c8c0cb3ba92fc89bad6dc8327f0bcdceeaa3eb3
olekvernberg <ole.christian.kvernberg@nav.no>
2025-11-21 08:38:48 +0100

    Setter opp test med spring context

--- src/test/kotlin/no/nav/gjenlevende/bs/sak/GjenlevendeBsSakApplicationTests.kt
+++ src/test/kotlin/no/nav/gjenlevende/bs/sak/GjenlevendeBsSakApplicationTests.kt
@@ -3,8 +3,10 @@ package no.nav.gjenlevende.bs.sak
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-class GjenlevendeBsSakApplicationTests : ApplicationLocal() {
+class GjenlevendeBsSakApplicationTests : SpringContextTest() {
+
     @Test
     fun contextLoads() {
+
     }
 }
---
+++ src/test/kotlin/no/nav/gjenlevende/bs/sak/SpringContextTest.kt
@@ -0,0 +1,27 @@
+package no.nav.gjenlevende.bs.sak
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import org.junit.jupiter.api.AfterEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(classes = [ApplicationLocal::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(
+    "integrasjonstest",
+)
+open class SpringContextTest {
+
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    @AfterEach
+    fun reset() {
+        resetWiremockServers()
+    }
+
+    private fun resetWiremockServers() {
+        applicationContext.getBeansOfType(WireMockServer::class.java).values.forEach(WireMockServer::resetRequests)
+    }
+}

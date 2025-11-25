d98ecd6d13e472a477b165c12aea9ea89b550d41
krikof-nav <kristian.kofoed@nav.no>
2025-11-25 13:33:36 +0100

    * Fjerner secureLogs implementasjon og referanser.
    * Skrur på Team Logs med test logging, skal fjernes etter verifisert log.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/Application.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/Application.kt
@@ -1,11 +1,16 @@
 package no.nav.gjenlevende.bs.sak
 
+import org.slf4j.LoggerFactory
+import org.slf4j.MarkerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 open class Application
 
+private val logger = LoggerFactory.getLogger(Application::class.java)
+
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+    logger.info(MarkerFactory.getMarker("TEAM_LOGS"), "Gjenlevende-BS-Sak bruker Team Logs, Bink Bonk!")
 }

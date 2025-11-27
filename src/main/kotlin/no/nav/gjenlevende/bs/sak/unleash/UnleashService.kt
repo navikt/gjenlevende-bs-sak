35f0614cf42661f0ff3920b64f741b9d2b07699e
krikof-nav <kristian.kofoed@nav.no>
2025-11-27 12:34:35 +0100

    Legger til !local profiling for Unleash, kan utbedres.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/unleash/UnleashService.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/unleash/UnleashService.kt
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
-@Profile("dev")
+@Profile("dev & !local")
 @Service
 class UnleashService(
     private val unleash: Unleash,

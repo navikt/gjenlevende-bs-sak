79a3ef4c1ab8c4d8fcc8d8c9cc5a35893262f244
olekvernberg <ole.christian.kvernberg@nav.no>
2025-12-02 21:33:39 +0100

    Lagt til foreign key mellom fagsak og person Fjernet tabell som ikke trengs Skiller mellom dev og prod på fagsak_ekstern_id slik at de ikke får konflikt mellom dev og prod, som kan være et problem grunnet jevnlig datalast til q1.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/config/DatabaseConfig.kt
@@ -2,12 +2,16 @@ package no.nav.gjenlevende.bs.sak.config
 
 import no.nav.familie.prosessering.PropertiesWrapperTilStringConverter
 import no.nav.familie.prosessering.StringTilPropertiesWrapperConverter
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.flyway.autoconfigure.FlywayConfigurationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import javax.sql.DataSource
+import kotlin.collections.contains
 
 @Configuration
 @EnableJdbcRepositories("no.nav.familie", "no.nav.gjenlevende")
@@ -20,4 +24,21 @@ open class DatabaseConfig : AbstractJdbcConfiguration() {
             PropertiesWrapperTilStringConverter(),
             StringTilPropertiesWrapperConverter(),
         )
+
+    @Bean
+    open fun verifyIgnoreIfProd(
+        @Value("\${spring.flyway.placeholders.ignoreIfProd}") ignoreIfProd: String,
+        environment: Environment,
+    ): FlywayConfigurationCustomizer {
+        val isProd = environment.activeProfiles.contains("prod")
+        val ignore = ignoreIfProd == "--"
+        return FlywayConfigurationCustomizer {
+            if (isProd && !ignore) {
+                throw RuntimeException("Prod profile men har ikke riktig verdi for placeholder ignoreIfProd=$ignoreIfProd")
+            }
+            if (!isProd && ignore) {
+                throw RuntimeException("Profile=${environment.activeProfiles} men har ignoreIfProd=--")
+            }
+        }
+    }
 }

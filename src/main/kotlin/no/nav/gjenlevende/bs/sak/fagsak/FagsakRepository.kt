cd78cca321ebc26e0b5a61704daf99a71aef1989
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-12-03 12:25:38 +0100

    Skal hente fagsak hvis den eksisterer, ikke inserte på nytt.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/fagsak/FagsakRepository.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/fagsak/FagsakRepository.kt
@@ -3,10 +3,18 @@ package no.nav.gjenlevende.bs.sak.fagsak
 import no.nav.gjenlevende.bs.sak.fagsak.domain.FagsakDomain
 import no.nav.gjenlevende.bs.sak.felles.InsertUpdateRepository
 import no.nav.gjenlevende.bs.sak.felles.RepositoryInterface
+import no.nav.gjenlevende.bs.sak.infotrygd.dto.StønadType
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
 interface FagsakRepository :
     RepositoryInterface<FagsakDomain, UUID>,
-    InsertUpdateRepository<FagsakDomain>
+    InsertUpdateRepository<FagsakDomain> {
+    @Query("SELECT * FROM fagsak WHERE fagsak_person_id = :fagsakPersonId AND stonadstype = :stønadstype")
+    fun findByFagsakPersonIdAndStønadstype(
+        fagsakPersonId: UUID,
+        stønadstype: StønadType,
+    ): FagsakDomain?
+}

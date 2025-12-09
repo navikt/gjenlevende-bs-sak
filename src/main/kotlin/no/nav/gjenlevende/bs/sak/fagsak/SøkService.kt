aae4395205cd2007c47b69f899cbab1e65a10914
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-12-09 13:47:58 +0100

    Laget controller og service for å hente info i søkefelt for en personident.

---
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/fagsak/SøkService.kt
@@ -0,0 +1,32 @@
+package no.nav.gjenlevende.bs.sak.fagsak
+
+import no.nav.gjenlevende.bs.sak.fagsak.domain.FagsakPerson
+import no.nav.gjenlevende.bs.sak.pdl.Navn
+import no.nav.gjenlevende.bs.sak.pdl.PdlService
+import org.springframework.stereotype.Service
+
+@Service
+class SøkService(
+    private val fagsakPersonService: FagsakPersonService,
+    private val pdlService: PdlService,
+) {
+    fun søkPerson(personident: String): Søkeresultat {
+        val fagsakPerson = fagsakPersonService.finnPerson(setOf(personident))
+        val navnPdl = pdlService.hentNavn(personident)
+
+        return tilSøkeresultat(personident, fagsakPerson, navnPdl)
+    }
+
+    private fun tilSøkeresultat(
+        personident: String,
+        fagsakPerson: FagsakPerson?,
+        navnPdl: Navn?,
+    ): Søkeresultat =
+        Søkeresultat(
+            navn = navnPdl?.let { "${it.fornavn} ${it.etternavn}" } ?: "Ukjent navn",
+            personident = personident,
+            fagsakPersonId = fagsakPerson?.id,
+            harTilgang = true, // TODO: implementer tilgangskontroll senere
+            harFagsak = fagsakPerson != null,
+        )
+}

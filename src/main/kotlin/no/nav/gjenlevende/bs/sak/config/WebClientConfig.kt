0a39ea8e85a8d952574e8b3b4ad933107a59593a
olekvernberg <ole.christian.kvernberg@nav.no>
2025-12-11 16:57:35 +0100

    Legg til OAuth2AuthorizedClientManager og OAuth2AuthorizedClientService

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/config/WebClientConfig.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/config/WebClientConfig.kt
@@ -3,6 +3,12 @@ package no.nav.gjenlevende.bs.sak.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -17,4 +23,24 @@ open class WebClientConfig {
             .baseUrl(infotrygdUrl)
             .defaultHeader("Content-Type", "application/json")
             .build()
+
+    @Bean
+    open fun authorizedClientService(
+        clientRegistrationRepository: ClientRegistrationRepository,
+    ): OAuth2AuthorizedClientService = InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository)
+
+    @Bean
+    open fun authorizedClientManager(
+        clientRegistrationRepository: ClientRegistrationRepository,
+        authorizedClientService: OAuth2AuthorizedClientService,
+    ): OAuth2AuthorizedClientManager {
+        val provider =
+            OAuth2AuthorizedClientProviderBuilder
+                .builder()
+                .clientCredentials()
+                .build()
+
+        return AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService)
+            .also { it.setAuthorizedClientProvider(provider) }
+    }
 }

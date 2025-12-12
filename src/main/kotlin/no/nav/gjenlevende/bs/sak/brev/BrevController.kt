5dd2ad11c014c502acd41f4b8ef21bd9c0615ed7
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-12-12 09:36:03 +0100

    brev endepunkt test

---
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/brev/BrevController.kt
@@ -0,0 +1,24 @@
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/api/brev"])
+class BrevController {
+    @PostMapping("/test")
+    fun lagSaksbehandlerbrev(
+        @RequestBody brevRequest: BrevRequest,
+    ): ResponseEntity<String> = ResponseEntity.ok("OK")
+}
+
+data class BrevRequest(
+    val brevMal: String,
+    val fritekstBolker: List<FritekstBolk>,
+)
+
+data class FritekstBolk(
+    val deltittel: String,
+    val innhold: String,
+)
---
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/brev/BrevService.kt
@@ -0,0 +1,6 @@
+package no.nav.gjenlevende.bs.sak.brev
+
+import org.springframework.stereotype.Service
+
+@Service
+class BrevService

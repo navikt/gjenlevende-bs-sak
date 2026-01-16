17259584fb1521d5c3e1271edd28b87eca208c33
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-01-16 10:48:03 +0100

    Mer pixelpushing

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/brev/BrevUtils.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/brev/BrevUtils.kt
@@ -29,12 +29,7 @@ fun lagHtml(request: BrevRequest): String {
             val underoverskrift = bolk.underoverskrift?.let { "<h2>$it</h2>" } ?: ""
             "<section class=\"avslutning\">\n${underoverskrift}\n<p>${bolk.innhold}</p>\n</section>\n"
         }
-    // .logo {
-//    position: absolute;
-//    top: 32pt;
-//    height: 16pt;
-//    width: auto;
-// }
+
     return """
         <!DOCTYPE html>
         <html lang="no">
@@ -49,14 +44,14 @@ fun lagHtml(request: BrevRequest): String {
                 h2 { font-size: 13pt; line-height: 16pt; font-weight: 700; margin-bottom: 6pt;}
                 h3 { font-size: 12pt; line-height: 16pt; font-weight: 700; margin-bottom: 6pt;}
                 section { margin-bottom: 26pt; }
+                .header {
+                    padding-top: 32pt;
+                    margin-bottom: 32pt
+                }
                 .logo {
                     display: block;
-                    padding-bottom: 32pt;
-                .header {
-                    position: relative;
-                    padding-top: 64pt;
+                    margin-bottom: 24pt;
                 }
-
             </style>
         </head>
         <body>

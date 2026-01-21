ad223c284d957ff229663387468173010e06e193
ma10s <mattis.janitz@nav.no>
2026-01-21 14:49:44 +0100

    Wip: Vil bruke maskintoken Vil åpne for azure-token-generator for å lage m2m token for testing i preprod

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/oppgave/OppgaveClient.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/oppgave/OppgaveClient.kt
@@ -1,69 +1,67 @@
 package no.nav.gjenlevende.bs.sak.oppgave
 
-import no.nav.gjenlevende.bs.sak.felles.OAuth2RestOperationsFactory
+import no.nav.gjenlevende.bs.sak.texas.TexasClient
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestOperations
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import org.springframework.web.util.UriComponentsBuilder
+import reactor.core.publisher.Mono
 import java.net.URI
+import java.time.Duration
 
 @Component
 class OppgaveClient(
+    private val webClient: WebClient,
+    private val texasClient: TexasClient,
+
     @Value("\${OPPGAVE_URL}")
     private val oppgaveUrl: URI,
-    @Value("\${oppgave.oauth.registration-id}")
-    private val registrationId: String,
-    oauth2RestFactory: OAuth2RestOperationsFactory,
+    @Value("\${OPPGAVE_SCOPE}")
+    private val oppgaveScope: URI,
+
 ) {
     private val logger = LoggerFactory.getLogger(OppgaveClient::class.java)
 
-    private val restTemplate: RestOperations = oauth2RestFactory.create(registrationId)
+    companion object {
+        private const val TIMEOUT_SEKUNDER = 10L
+        private const val API_BASE_URL = "/api/v1/oppgaver"
+    }
 
     fun opprettOppgave(oppgave: Oppgave) : Oppgave{
         logger.info("Lag oppgave=${oppgave}")
         val uri = lagBehandleSakOppgaveURI()
         logger.info("Sender opprettOppgave request til Oppgave-service ")
+        val maskinToken = texasClient.hentMaskinToken(oppgaveScope.toString())
 
-        try {
-            val exchange = restTemplate.exchange(
-               uri,
-                HttpMethod.POST,
-                HttpEntity(oppgave, httpHeaders()),
-                object : ParameterizedTypeReference<Oppgave>() {}
-            )
 
-            val oppgaveResponse : Oppgave =
-                exchange.body
-                    ?: throw RuntimeException("Ikke body i response fra oppgave")
 
-            require(oppgaveResponse.id != null && oppgaveResponse.id > 0) {
-                "Oppgave id mangler i response fra oppgave"
-            }
-
-            return oppgaveResponse
-
-        }catch (ex: Exception){
-            throw RuntimeException("Feil ved oppretting av oppgave for behandling=${oppgave}", ex)
-        }
+        return webClient
+            .post()
+            .uri(uri)
+            .header( "Authorization", "Bearer $maskinToken")
+            .header("X-Correlation-ID", MDC.get("callId") ?: "test")
+            .bodyValue(oppgave)
+            .retrieve()
+            .bodyToMono<Oppgave>()
+            .switchIfEmpty(Mono.error(NoSuchElementException("Tom respons fra oppgave")))
+            .timeout(Duration.ofSeconds(TIMEOUT_SEKUNDER))
+            .doOnNext { response ->
+                logger.info("Oppgave opprettet med id: ${response.id} ")
+            }.doOnError {
+                logger.error("Feil: å hente perioder for person: $it")
+            }.block() ?: throw RuntimeException("Klarte ikke opprette oppgave")
 
     }
 
     private fun lagBehandleSakOppgaveURI(): URI = UriComponentsBuilder
         .fromUri(oppgaveUrl)
-        .path("/api/v1/oppgaver")
+        .path(API_BASE_URL)
         .build()
         .toUri()
 
 
 }
-
-private fun httpHeaders(): HttpHeaders =
-    HttpHeaders().apply {
-        add("X-Correlation-ID", MDC.get("callId"))
-    }

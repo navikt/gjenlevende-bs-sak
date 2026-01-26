493e09c6e69ba89d5621e88198e4a3bf8958db5f
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-01-26 16:14:09 +0100

    Revert "Prøver med annet pattern for client"
    This reverts commit 2056d518a9da7866d0a402736d7839d3f0b15262.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/saksbehandler/EntraProxyClient.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/saksbehandler/EntraProxyClient.kt
@@ -1,43 +1,49 @@
 package no.nav.gjenlevende.bs.sak.saksbehandler
 
 import SaksbehandlerResponse
-import no.nav.gjenlevende.bs.sak.felles.OAuth2RestOperationsFactory
+import no.nav.gjenlevende.bs.sak.texas.TexasClient
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestOperations
-import org.springframework.web.util.UriComponentsBuilder
-import java.net.URI
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.time.Duration
 
 @Service
 class EntraProxyClient(
-    @Value("\${ENTRA_PROXY_URL}") private val entraProxyUrl: URI,
-    @Value("\${entra-proxy.oauth.registration-id}") registrationId: String,
-    oauth2RestFactory: OAuth2RestOperationsFactory,
+    @Value("\${ENTRA_PROXY_URL}") private val entraProxyUrl: String,
+    @Value("\${ENTRA_PROXY_SCOPE}") private val entraProxyAudience: String,
+    private val texasClient: TexasClient,
 ) {
     private val logger = LoggerFactory.getLogger(EntraProxyClient::class.java)
-    private val restTemplate: RestOperations = oauth2RestFactory.create(registrationId)
+    private val webClient =
+        WebClient
+            .builder()
+            .baseUrl(entraProxyUrl)
+            .build()
+
+    companion object {
+        private const val TIMEOUT_SEKUNDER = 10L
+    }
 
     fun hentSaksbehandlerInfo(navIdent: String): SaksbehandlerResponse {
-        val uri =
-            UriComponentsBuilder
-                .fromUri(entraProxyUrl)
-                .pathSegment("api", "v1", "ansatt", navIdent)
-                .build()
-                .toUri()
-
-        logger.info("Henter saksbehandlerinfo for navIdent: $navIdent")
-
-        val response =
-            restTemplate.exchange(
-                uri,
-                HttpMethod.GET,
-                null,
-                SaksbehandlerResponse::class.java,
+        val oboToken =
+            texasClient.hentOboToken(
+                targetAudience = entraProxyAudience,
             )
 
-        return response.body
-            ?: throw RuntimeException("Klarte ikke å hente saksbehandlerinfo fra entra-proxy")
+        logger.info("OBO token: $oboToken")
+
+        return webClient
+            .get()
+            .uri("/api/v1/ansatt/$navIdent")
+            .header("Authorization", "Bearer $oboToken")
+            .retrieve()
+            .bodyToMono<SaksbehandlerResponse>()
+            .timeout(Duration.ofSeconds(TIMEOUT_SEKUNDER))
+            .doOnNext { response ->
+                logger.info("Hentet saksbehandlerinfo for navIdent: ${response.navIdent}")
+            }.doOnError { logger.error("Feilet å hente saksbehandlerinfo: $it") }
+            .block() ?: throw RuntimeException("Klarte ikke å hente saksbehandlerinfo fra entra-proxy")
     }
 }

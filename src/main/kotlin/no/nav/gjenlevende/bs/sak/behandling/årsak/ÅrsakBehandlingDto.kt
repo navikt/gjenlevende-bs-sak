af93ab81907f31a565adc8c4a1a8c24b4540ba45
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-01-28 08:46:07 +0100

    Legger til jackson json property på årsak som forteller eksplisitt hva navnet på propertyen skal være.

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/behandling/årsak/ÅrsakBehandlingDto.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/behandling/årsak/ÅrsakBehandlingDto.kt
@@ -1,11 +1,13 @@
 package no.nav.gjenlevende.bs.sak.behandling.årsak
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 import java.util.UUID
 
 data class ÅrsakBehandlingDto(
     val behandlingId: UUID,
     val kravdato: LocalDate,
+    @JsonProperty("årsak")
     val årsak: Årsak,
     val beskrivelse: String,
 )

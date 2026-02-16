41edec3ffbc98a9b89e7369a5bcfcc9b92634494
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-02-16 14:07:19 +0100

    dokarkivclient, journalførvedtaksbrevtask og domeneclasser

---
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/iverksett/DokarkivClient.kt
@@ -0,0 +1,47 @@
+package no.nav.gjenlevende.bs.sak.iverksett
+
+import no.nav.gjenlevende.bs.sak.iverksett.domene.ArkiverDokumentRequest
+import no.nav.gjenlevende.bs.sak.texas.TexasClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.net.URI
+
+@Component
+class DokarkivClient(
+    @Value("\${DOKARKIV_URL}") private val dokarkivUrl: URI,
+    @Value("\${DOKARKIV_SCOPE}") private val dokarkivScope: URI,
+    private val texasClient: TexasClient,
+) {
+    val webClient =
+        WebClient
+            .builder()
+            .baseUrl(dokarkivUrl.toString())
+            .defaultHeader("Content-Type", "application/json")
+            .build()
+
+    fun arkiverDokument(arkiverDokumentRequest: ArkiverDokumentRequest): String {
+        val headers =
+            HttpHeaders().apply {
+                setBearerAuth(texasClient.hentMaskinToken(targetAudience = dokarkivScope.toString()))
+                this.contentType = MediaType.TEXT_HTML
+                this.accept = listOf(MediaType.APPLICATION_PDF)
+            }
+
+        return webClient
+            .post()
+            .uri(OPPRETT_JOURNALPOST)
+            .headers { it.addAll(headers) }
+            .bodyValue(arkiverDokumentRequest) // TODO response av noe slag her? journalpostid elns
+            .retrieve()
+            .bodyToMono<String>()
+            .block() ?: error("Ingen response ved henting av tilgang til person med relasjoner")
+    }
+
+    companion object {
+        const val OPPRETT_JOURNALPOST = "rest/journalpostapi/v1/opprettJournalpost"
+    }
+}
--- src/main/kotlin/no/nav/gjenlevende/bs/sak/iverksett/brev/JournalførVedtaksbrevTask.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/iverksett/brev/JournalførVedtaksbrevTask.kt
@@ -3,7 +3,16 @@ package no.nav.gjenlevende.bs.sak.iverksett.brev
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
-import no.nav.familie.prosessering.internal.TaskService
+import no.nav.gjenlevende.bs.sak.behandling.BehandlingService
+import no.nav.gjenlevende.bs.sak.brev.BrevService
+import no.nav.gjenlevende.bs.sak.fagsak.FagsakPersonService
+import no.nav.gjenlevende.bs.sak.fagsak.FagsakRepository
+import no.nav.gjenlevende.bs.sak.iverksett.DokarkivClient
+import no.nav.gjenlevende.bs.sak.iverksett.domene.ArkiverDokumentRequest
+import no.nav.gjenlevende.bs.sak.iverksett.domene.Dokument
+import no.nav.gjenlevende.bs.sak.iverksett.domene.Dokumenttype
+import no.nav.gjenlevende.bs.sak.iverksett.domene.Filtype
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -15,17 +24,63 @@ import java.util.UUID
     beskrivelse = "Journalfører vedtaksbrev.",
 )
 class JournalførVedtaksbrevTask(
-    private val taskService: TaskService,
+    private val behandlingService: BehandlingService,
+    private val fagsakRepository: FagsakRepository,
+    private val fagsakPersonService: FagsakPersonService,
+    private val brevService: BrevService,
+    private val dokarkivClient: DokarkivClient,
 ) : AsyncTaskStep {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override fun doTask(task: Task) {
         val behandlingId = UUID.fromString(task.payload)
-    }
-
-    override fun onCompletion(task: Task) {
-//        taskService.save(task.opprettNesteTask())
+        val behandling =
+            behandlingService.hentBehandling(behandlingId)
+                ?: error("Fant ikke behandling med id=$behandlingId")
+        val fagsak =
+            fagsakRepository.findById(behandling.fagsakId).orElseThrow {
+                error("Fant ikke fagsak med id=${behandling.fagsakId}")
+            }
+        val personident = fagsakPersonService.hentAktivIdent(fagsak.fagsakPersonId)
+        val vedtaksbrev =
+            brevService.hentBrev(behandlingId)
+                ?: error("Fant ikke brev for behandlingId=$behandlingId")
+        val brevPdf = vedtaksbrev.brevPdf ?: error("Vedtaksbrev mangler PDF for behandlingId=$behandlingId")
+        val dokument =
+            Dokument(
+                dokument = brevPdf,
+                filtype = Filtype.PDFA,
+                dokumenttype = Dokumenttype.BARNETILSYNSTØNAD_VEDTAK, // TODO utlede
+                tittel = "Test-tittel", // TODO ikke dette
+            )
+        val dokarkivResponse =
+            dokarkivClient.arkiverDokument(
+                ArkiverDokumentRequest(
+                    fnr = personident, // TODO Er dette alltid ett fnr? Er det riktig å bruke personident?
+                    `forsøkFerdigstill` = true,
+                    hoveddokumentvarianter = listOf(dokument),
+                    vedleggsdokumenter = emptyList(), // TODO
+                    fagsakId = fagsak.eksternId.toString(),
+                    `journalførendeEnhet` = "", // TODO
+//                `førsteside` = TODO,
+                    eksternReferanseId = "$behandlingId-vedtaksbrev",
+//                avsenderMottaker = "", TODO
+                ),
+            )
+        // TODO lagre journalpostId og dokumentId fra dokarkivResponse i iverksettResultat
     }
 
     companion object {
         const val TYPE = "journalførVedtaksbrev"
+
+        fun opprettTask(payload: String): Task =
+            Task(
+                TYPE,
+                payload,
+            )
     }
+
+    data class JournalførVedtaksbrevTaskData(
+        val behandlingId: UUID,
+    )
 }

eb598b8d179680bd500951b59630ef838521cea3
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-02-26 16:32:49 +0100

    Legger til beslutterenhet

--- src/main/kotlin/no/nav/gjenlevende/bs/sak/iverksett/Iverksett.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/sak/iverksett/Iverksett.kt
@@ -11,6 +11,7 @@ data class Iverksett(
     @Id
     val behandlingId: UUID,
     val eksternReferanseId: UUID? = null,
+    val beslutterEnhet: String,
     val journalpostResultatId: String? = null,
     @Embedded(onEmpty = Embedded.OnEmpty.USE_EMPTY)
     val sporbar: Sporbar = Sporbar(),

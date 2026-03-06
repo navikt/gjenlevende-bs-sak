9dc2c518aad7058572c1bc84815e55dd868037d4
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-03-06 13:35:29 +0100

    Tar in db-endringer fra annen pr

---
+++ src/main/resources/db/migration/V28__endre_behandling_barn.sql
@@ -0,0 +1,6 @@
+ALTER TABLE behandling_barn
+    DROP COLUMN IF EXISTS soknad_barn_id;
+ALTER TABLE behandling_barn
+    ADD COLUMN IF NOT EXISTS fodsel_dato DATE;
+ALTER TABLE behandling_barn
+    ADD COLUMN IF NOT EXISTS hentet_tidspunkt TIMESTAMP(3);
